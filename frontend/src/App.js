35b6766c1f24ee208f01b82b3c5f306620ccd35e
pavan-nambi <18B91A12A2@srkrec.ac.in>
2022-06-08 09:23:39 +0530

    rewamped frontend totally as frontegg seems easy for google login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,55 @@
-// import logo from './logo.svg';
 import "./App.css";
-import FullCalendar from "@fullcalendar/react"; // must go before plugins
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-import interactionPlugin from "@fullcalendar/interaction";
+import { useEffect } from "react";
+import { ContextHolder } from "@frontegg/rest-api";
+import { useAuth, useLoginWithRedirect } from "@frontegg/react";
+import { AdminPortal } from "@frontegg/react";
 
 function App() {
-  const handleDateClick = (e) => {
-    console.log(e);
+  const { user, isAuthenticated } = useAuth();
+  const loginWithRedirect = useLoginWithRedirect();
+
+  // Uncomment this to redirect to login automatically
+  useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, loginWithRedirect]);
+
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+  };
+
+  const handleClick = () => {
+    AdminPortal.show();
   };
 
   return (
-    <>
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        dateClick={handleDateClick}
-      />
-    </>
+    <div className="App">
+      {isAuthenticated ? (
+        <div>
+          <div>
+            <img src={user?.profilePictureUrl} alt={user?.name} />
+          </div>
+          <div>
+            <span>Logged in as: {user?.name}</span>
+          </div>
+          <div>
+            <button onClick={() => alert(user.accessToken)}>
+              What is my access token?
+            </button>
+          </div>
+          <div>
+            <button onClick={() => logout()}>Click to logout</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => loginWithRedirect()}>Click me to login</button>
+        </div>
+      )}
+      <button onClick={handleClick}>Settings</button>
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { FronteggProvider } from "@frontegg/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const contextOptions = {
+  baseUrl: "https://app-2annv5vbbpfb.frontegg.com",
+  clientId:
+    "464003919671-a2aukfoovtj228c99ojef1g1083i9mdp.apps.googleusercontent.com-8a8a-47d7-9d74-b425f3ea9a28",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
     <App />
-  </React.StrictMode>
+  </FronteggProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
